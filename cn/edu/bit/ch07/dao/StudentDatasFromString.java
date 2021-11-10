05b1ec2a703d029f4b6786c5ba6d8cde134fbff3
Olivia <laiyunghwei@gmail.com>
2021-11-11 01:48:53 +0800

    Create StudentDatasFromString.java

---
+++ cn/edu/bit/ch07/dao/StudentDatasFromString.java
@@ -0,0 +1,8 @@
+package cn.edu.bit.ch07.dao;
+
+public class StudentDatasFromString extends StudentDatas {
+    @Override
+    public String getStudents() throws Exception {
+        throw new Exception("暂时不支持这种方式");
+    }
+}
