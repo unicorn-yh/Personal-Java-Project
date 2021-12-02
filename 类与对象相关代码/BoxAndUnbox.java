6569b1522fdeaf7e73e813d9449a99df63c88a30
unicornftk <laiyunghwei@gmail.com>
2021-12-03 00:46:42 +0800

    Create BoxAndUnbox.java

---
+++ 类与对象相关代码/BoxAndUnbox.java
@@ -0,0 +1,16 @@
+
+public class BoxAndUnbox {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int value=100;
+
+		Integer obj=value;  //装箱
+
+		int result=obj*2;     //拆箱
+
+	}
+
+}
