fe6a4abaf3febd261ae6ca0451371fbcf54c8954
unicornftk <laiyunghwei@gmail.com>
2022-03-14 10:06:11 +0800

    Create test1.java

---
+++ mydefault/test1.java
@@ -0,0 +1,31 @@
+class MyException extends Exception {
+	private int idnumber;
+	public MyException(String message,int id) {
+		super(message);
+		this.idnumber=id;
+	}
+	public int getId() {
+		return idnumber;
+	}
+}
+
+public class test1 {
+	public void regist(int num)throws MyException{
+		if(num<0) {
+			System.out.println("登记号码"+num);
+			throw new MyException("号码为负值，不合理",3);
+		}
+	}
+	public void manager() {
+		try {
+			regist(-100);
+		}catch(MyException e) {
+			System.out.println("登记失败，出错种类"+ e.getId());
+		}
+		System.out.println("本次登记操作结束");
+	}
+	public static void main(String[]args) {
+		test1 t=new test1();
+		t.manager();
+	}
+}
