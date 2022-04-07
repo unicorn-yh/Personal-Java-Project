ff3c0101cbf4df97e8d8d3556fe8a438bd2434d9
unicornftk <laiyunghwei@gmail.com>
2022-04-08 07:52:31 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,41 @@
+import java.util.*;
 import java.io.*;
-public class test {
- public static void main(String[]args) {
-  char c='\0';
-        System.out.print("Please input a char:");
+import java.net.*;
+
+
+public class test{
+    public static void main(String args[]){
+        String dir="c:/users/user/desktop/java-1223/url.html";
+        String dir2="d:/url.txt";
+
         try{
-        c=(char)System.in.read();
-        }catch(IOException e){}
-        System.out.println("You have entered:"+c);
+            File fread=new File("C:/Users/User/Desktop/java-1223/project_dataset/swissprot_data11.tsv");
+            BufferedReader br=new BufferedReader(new FileReader(fread));
+            //BufferedWriter bw=new BufferedWriter(new FileWriter(dir));
+            //PrintWriter pw=new PrintWriter(bw);
+            //BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dir2)));
+            String temp=null,s;
+            StringBuffer sb=new StringBuffer();
+            
+            while((temp=br.readLine())!=null){
+                //StringTokenizer st= new StringTokenizer(temp,"\t");
+                //while(st.hasMoreTokens()){
+                    //s=st.nextToken();
+                    for(int i=0;i<=temp.length();i++){
+                        System.out.print(temp.split("\t")[i]+"*");
+                    }
+                    //System.out.println(temp.split("\t")[2]);
+                //}
+              System.out.println("\n");
+            }
+            
+            //System.out.println("----------------------------------");
+            //System.out.println("网页"+dir+"的内容保存完成，保存在"+dir+"文件中，请注意查看");
+            br.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
