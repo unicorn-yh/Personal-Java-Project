a785e345d3039a5666e58781cff1a34ab836842d
unicornftk <laiyunghwei@gmail.com>
2022-04-08 07:52:27 +0800

    Update simple_calculator.java

--- simple_calculator.java
+++ simple_calculator.java
@@ -1,129 +1,224 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
-class Calculator extends JFrame{
-	String a[]={"7","8","9","/","4","5","6","*","1","2","3","-","0",".","=","+"};
-	JButton b[]=new JButton[a.length];
-	JButton b1=new JButton("DEL");
-	JButton b2=new JButton("AC");
-	JTextField tf=new JTextField("0",20);
-	JPanel p1=new JPanel(new GridLayout(1,2,10,10));
-	JPanel p2=new JPanel(new GridLayout(4,4,5,5));
-	JPanel p3=new JPanel(new BorderLayout(5,5));
-	double result=0;
-	
-	Calculator(){
-		setTitle("Calculator");
-		setBounds(100,100,400,400);
-		setVisible(true);
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container c=this.getContentPane();
-		setLayout(new BorderLayout(10,5));
-		tf.setHorizontalAlignment(JTextField.RIGHT);
-		tf.setEditable(false);
-		tf.setBackground(Color.white);
-		init();
-		p3.add(p1,BorderLayout.NORTH);
-		p3.add(p2,BorderLayout.CENTER);
-		add(tf,BorderLayout.NORTH);
-		add(p3);
+import javax.swing.*;
+ 
+public class simple_calculator {
+ 
+	public static void main(String[] args) {
+		new MyFrame("计算器");
 	}
-	void init(){
-		boolean c=false;
-		for(int i=0;i<a.length;i++){
-			b[i]= new JButton(a[i]);
-			p2.add(b[i]);
-			b[i].addActionListener(new num());
-		}
-		p1.add(b1);
-		b1.addActionListener(new num());
-		p1.add(b2);
-		b2.addActionListener(new num());
-	}
-	class num implements ActionListener{
-		public void actionPerformed(ActionEvent e){
-			String s=e.getActionCommand();
-			if(s!="=" && s!="AC" && s!="DEL"){
-				if(tf.getText().equals("0"))
-					tf.setText(s);
-				else
-					tf.setText(tf.getText()+s);	
-			}
-			else if(s.equals("=")){
-				ans();
-			}
-			else if(s.equals("AC")){
-				tf.setText("0");
-			}
-			else if(s.equals("DEL")){
-				if(tf.getText().length()>1)
-					tf.setText(tf.getText().substring(0, tf.getText().length()-1));
-				else
-					tf.setText("0");
-			}
-		}
-	}
-	void ans(){
-		int j=0,k=0;
-		double t=0;
-		StringBuffer number=new StringBuffer();
-		String arrayNum[]=new String[10];
-		String arraySign[]=new String[10];
-		String s1=tf.getText();
-		char c[]=s1.toCharArray();
-		for(int i=0;i<c.length;i++){
-			if((c[i]+"").matches("[0-9]")|| String.valueOf(c[i]).equals(".")){
-				number.append(c[i]);
-				arrayNum[j]=new String(number);
-			}
-			else {
-				j++;
-				number.delete(0,i);
-				arraySign[k]=String.valueOf(c[i]);
-				k++;
-			}	
-		}
-		
-		if((s1.contains("*")||s1.contains("/"))&&(s1.indexOf("+")==-1)&&(s1.indexOf("-")==-1)){
-			for(int m=0;m<k;m++){
-				double d1=Double.parseDouble(arrayNum[m]);
-				double d2=Double.parseDouble(arrayNum[m+1]);
-				if(arraySign[m].equals("*")){
-					t=d1*d2;
-					arrayNum[m+1]=String.valueOf(t);	
-				}
-				else{
-					t=d1/d2;
-					arrayNum[m+1]=String.valueOf(t);
-				}
-			}
-			result=t;
-		}
-		else if((s1.contains("+")||s1.contains("-"))&&(s1.indexOf("*")==-1)&&(s1.indexOf("/")==-1)){
-			for(int m=0;m<k;m++){
-				double d1=Double.parseDouble(arrayNum[m]);
-				double d2=Double.parseDouble(arrayNum[m+1]);
-				if(arraySign[m].equals("+")){
-					t=d1+d2;
-					arrayNum[m+1]=String.valueOf(t);	
-				}
-				else{
-					t=d1-d2;
-					arrayNum[m+1]=String.valueOf(t);
-				}
-			}
-			result=t;
-		}
-		else
-			JOptionPane.showMessageDialog(null, "ERROR!");
-		tf.setText(String.valueOf(result));
-	}	
+ 
 }
-
-public class simple_calculator{
-	public static void main(String []args){
-		new Calculator();
+class MyFrame extends Frame{
+	private static final long serialVersionUID = 1L;
+ 	private TextArea ta,tb;
+	public MyFrame(String title){
+		super(title);
+		SetTextAreas();
+		SetButtonArea();
+		SetMainFram();
+	}
+ 
+	private void SetButtonArea() {
+		
+		addButton("7",33,28,20,178);
+		addButton("8",33,28,58,178);
+		addButton("9",33,28,96,178);
+		addButton("/",33,28,134,178);
+		addButton("%",33,28,172,178);
+			
+		addButton("4",33,28,20,210);
+		addButton("5",33,28,58,210);
+		addButton("6",33,28,96,210);
+		addButton("*",33,28,134,210);
+		addButton("1/x",33,28,172,210);
+		
+		addButton("1",33,28,20,242);
+		addButton("2",33,28,58,242);
+		addButton("3",33,28,96,242);
+		addButton("-",33,28,134,242);
+		
+		addButton("0",71,28,20,274);
+		addButton(".",33,28,96,274);
+		addButton("+",33,28,134,274);
+			
+		addButton("=",33,60,172,242);
+	}
+ 
+	double m,n;
+	String  k;
+	boolean flag =true;
+	boolean flag2 =false;
+	private void addButton(String string, int i, int j,int x,int y) {
+		final Button b = new Button(string);
+		b.setLocation(x, y);
+		b.setSize(i, j);
+		b.setFont(new Font("标楷体", Font.BOLD, 15));
+		b.setBackground(Color.pink); 
+		b.setForeground(Color.darkGray);
+		b.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				counts();
+				
+			}
+ 
+			private void counts() {
+				if(ta.getText().equals("")&&(b.getActionCommand().equals("+")||
+						b.getActionCommand().equals("-")||
+						b.getActionCommand().equals("*")||
+						b.getActionCommand().equals("/")||
+						b.getActionCommand().equals("%")||
+						b.getActionCommand().equals("1/x")||
+						b.getActionCommand().equals("="))) {
+					
+				}else if(ta.getText().equals(".")&&(b.getActionCommand().equals("+")||
+						b.getActionCommand().equals("-")||
+						b.getActionCommand().equals("*")||
+						b.getActionCommand().equals("/")||
+						b.getActionCommand().equals("%")||
+						b.getActionCommand().equals("1/x")||
+						b.getActionCommand().equals("="))){
+					
+				}else {
+				if(		b.getActionCommand().equals("+")||
+						b.getActionCommand().equals("-")||
+						b.getActionCommand().equals("*")||
+						b.getActionCommand().equals("%")||
+						b.getActionCommand().equals("1/x")||
+						b.getActionCommand().equals("/")){
+					if(flag2 = true) {
+						flag2 = false;
+					}
+				if(flag) {
+					n = new Double(ta.getText()).doubleValue();
+					flag = false;
+				}else {
+					if(k=="="){
+						
+					}else {
+						m = new Double(ta.getText()).doubleValue();
+						if(k == "-") {
+							if(n==0)
+								n = m;
+							else
+								n=n-m;
+						}else if(k == "+") {
+							if(n==0)
+								n = m;
+							else
+								n=n+m;
+						}else if(k == "*") {
+							if(n==0)
+								n = m;
+							else
+								n=n*m;
+						}else if(k=="%"){
+							if(n==0)
+								n=m;
+							else
+								m=m*100;
+						}else if(k=="1/x"){
+							if(n==0)
+								n=m;
+							else
+								n=1/m;
+						}else if(k == "/") {
+							if(n==0)
+								n = m;
+							else
+								n=n/m;
+						}
+					}
+				}
+					k = b.getActionCommand();
+						ta.setText("");	
+				}else if(b.getActionCommand().equals("=")) {
+					m = new Double(ta.getText()).doubleValue();
+					if(k == "+") {
+						ta.setText("");
+						ta.append(n+"+"+m);
+						ta.append(System.getProperty("line.separator"));
+						n = n+m;
+						ta.append("="+n);
+					}else if(k == "-") {
+						ta.setText("");
+						ta.append(n+"-"+m);
+						ta.append(System.getProperty("line.separator"));
+						n = n-m;
+						ta.append("="+n);
+					}else if(k == "*") {
+						ta.setText("");
+						ta.append(n+"*"+m);
+						ta.append(System.getProperty("line.separator"));
+						n = n*m;
+						ta.append("="+n);
+					}else if(k == "%") {
+						ta.setText("");
+						ta.append("%"+m);
+						ta.append(System.getProperty("line.separator"));
+						m = m*100;
+						ta.append("="+m+"%");
+					}else if(k == "1/x") {
+						ta.setText("");
+						ta.append(1+"/"+m);
+						ta.append(System.getProperty("line.separator"));
+						n = 1/m;
+						ta.append("="+n);			
+					}else if(k == "/") {
+						ta.setText("");
+						ta.append(n+"/"+m);
+						ta.append(System.getProperty("line.separator"));
+						n= n/m;
+						ta.append("="+n);
+					}
+					k="=";
+					flag2 = true;
+				}else {
+					if(flag2) {
+						flag = true;
+						flag2 = false;
+						ta.setText("");
+						m = n =0;
+					}
+					ta.append(b.getActionCommand());
+				}
+			}
+		}
+	});	
+		this.add(b);
+	}
+	private void SetTextAreas() {
+		ta = new TextArea("0",8,52,3);
+		ta.setBackground(Color.lightGray);
+		ta.setSize(190, 50);
+		ta.setFont(new Font("标楷体", Font.BOLD, 15));
+		ta.setLocation(20,60);
+		this.add(ta);
+		tb = new TextArea("  DESIGN BY 海与怡",8,52,3);
+                tb.setBackground(Color.pink);
+                tb.setSize(190,25);
+                tb.setFont(new Font("标楷体", Font.BOLD, 16));
+                tb.setForeground(Color.blue);
+		tb.setLocation(20,130);
+		this.add(ta);
+		this.add(tb);
+	}
+	@SuppressWarnings("deprecation")
+	private void SetMainFram() {
+		this.setLayout(null);
+		this.setSize(220,310);
+		this.setVisible(true);
+		this.setLocation(310, 340);
+		this.setResizable(false);
+		ta.setEditable(false);
+		tb.setEditable(false);
+		this.setCursor(Cursor.HAND_CURSOR);
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});			
+		
 	}
 }
